60237104a1ea0e9c06416a04d7c573d03707ff47
Bahodir <buxorovbahodir11@gmail.com>
2024-12-17 15:18:02 +0500

    product page is done

--- lib/features/home/widgets/custom_clip_rect.dart
+++ lib/features/home/widgets/custom_clip_rect.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 
+import '../../product/product_page.dart';
+
 class CustomClipRRect extends StatelessWidget {
   final String? imgURL;
   final String? text;
@@ -15,76 +17,87 @@ class CustomClipRRect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 15),
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(8),
-                  topRight: Radius.circular(8),
-                ),
-                child: Image.asset(
-                  imgURL!,
-                  width: 200,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                top: 8,
-                right: 8,
-                child: GestureDetector(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.transparent,
-                      shape: BoxShape.circle,
-                    ),
-                    padding: const EdgeInsets.all(8),
-                    child: const Icon(
-                      IconsaxPlusBroken.heart,
-                      color: Colors.white,
-                      size: 24,
-                    ),
+    return GestureDetector(
+      onTap: () {
+        // ProductPage sahifasiga o'tish
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ProductPage(),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 15),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(8),
+                    topRight: Radius.circular(8),
+                  ),
+                  child: Image.asset(
+                    imgURL!,
+                    width: 200,
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ),
-            ],
-          ),
-          Container(
-            width: 200,
-            height: 70,
-            padding: const EdgeInsets.all(8.0),
-            decoration: const BoxDecoration(
-              color: Colors.white10,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(
-                  8,
-                ),
-                bottomRight: Radius.circular(
-                  8,
-                ),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  text!,
-                  style: const TextStyle(color: Colors.white),
-                ),
-                Text(
-                  "\$$price",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                Positioned(
+                  top: 8,
+                  right: 8,
+                  child: GestureDetector(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.transparent,
+                        shape: BoxShape.circle,
+                      ),
+                      padding: const EdgeInsets.all(8),
+                      child: const Icon(
+                        IconsaxPlusBroken.heart,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+            Container(
+              width: 200,
+              height: 70,
+              padding: const EdgeInsets.all(8.0),
+              decoration: const BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(
+                    8,
+                  ),
+                  bottomRight: Radius.circular(
+                    8,
+                  ),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    text!,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    "\$$price",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
